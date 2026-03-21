fcbd15d222691274e807e99f750cca08b09a39c2
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2026-03-20 23:51:09 -0300

    Method post added

--- src/main/java/com/clinicflow/clinic/controller/ClinicController.java
+++ src/main/java/com/clinicflow/clinic/controller/ClinicController.java
@@ -2,6 +2,8 @@ package com.clinicflow.clinic.controller;
 
 import com.clinicflow.clinic.dto.ClinicRequest;
 import com.clinicflow.clinic.dto.ClinicResponse;
+import com.clinicflow.clinic.service.ClinicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/clinic")
+@RequestMapping("/clinics")
 public class ClinicController {
 
+    @Autowired
+    private ClinicService clinicService;
+
     @PostMapping
-    public ResponseEntity<ClinicResponse> saveClinic(@RequestBody ClinicRequest clinicRequest) {
-        re
+    public ResponseEntity<ClinicResponse> createClinic (@RequestBody ClinicRequest clinicRequest) {
+        ClinicResponse response = clinicService.create(clinicRequest);
+        return ResponseEntity.status(200).body(response);
     }
 }
