f0459f751c8c4173496f9c457a74b0527ee9e323
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2026-03-20 23:53:45 -0300

    method create Created

--- src/main/java/com/clinicflow/clinic/service/ClinicService.java
+++ src/main/java/com/clinicflow/clinic/service/ClinicService.java
@@ -1,4 +1,25 @@
 package com.clinicflow.clinic.service;
 
+import com.clinicflow.clinic.dto.ClinicRequest;
+import com.clinicflow.clinic.dto.ClinicResponse;
+import com.clinicflow.clinic.mapper.ClinicMapper;
+import com.clinicflow.clinic.model.Clinic;
+import com.clinicflow.clinic.repository.ClinicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClinicService {
+
+    @Autowired
+    private ClinicMapper clinicMapper;
+    @Autowired
+    private ClinicRepository clinicRepository;
+
+    public ClinicResponse create (ClinicRequest request){
+        Clinic clinic = clinicMapper.toEntity(request); //crio uma clinica, chamando o método ToEntity
+        clinic.setActive(true); //seto status
+        Clinic savedClinic = clinicRepository.save(clinic); //Crio outra clinica e salvo com as informaçoes de antes
+        return clinicMapper.toResponse(savedClinic); // retorno a clinica salva
+    }
 }
